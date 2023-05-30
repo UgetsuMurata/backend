ae28826a242ee7e87c7471d076898991b9669e18
Kai Yaneza <92856337+yaneza-teamlight@users.noreply.github.com>
2023-05-31 05:56:15 +0800

    new things

--- penny_one/urls.py
+++ penny_one/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from penny_one import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_repair'),
+    path('api/token/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/users/', views.users, name="user"),
-    path('api/users/<int:id>', views.userInfo, name="user_info")
+    path('api/users/<int:id>', views.userInfo, name="user_info"),
+    path('api/ewallet/<int:id>', views.EWallet, name="ewallet")
 ]
--- penny_one/views.py
+++ penny_one/views.py
@@ -1,6 +1,6 @@
-from penny_one.models import user, user_info
+from penny_one.models import user, user_info, ewallet
 from django.http import JsonResponse
-from penny_one.serializer import UserSerializer, UserInfoSerializer
+from penny_one.serializer import UserSerializer, UserInfoSerializer, EwalletSerializer
 
 def users(request):
     data = user.objects.all()
@@ -9,9 +9,17 @@ def users(request):
 
 def userInfo(request, id):
     try:
-        data = user_info.objects.get(UserID=id)
+        data = user_info.objects.get(userID=id)
         serializer = UserInfoSerializer(data, many=False)
         return JsonResponse({'user_info': serializer.data})
     except user_info.DoesNotExist:
         return JsonResponse({'error': 'User info not found'}, status=404)
     
+def EWallet(request, id):
+    try:
+        data = ewallet.objects.get(walletID=id)
+        serializer = EwalletSerializer(data, many=False)
+        return JsonResponse({'user_info': serializer.data})
+    except user_info.DoesNotExist:
+        return JsonResponse({'error': 'ewallet not found'}, status=404)
+
